e0b3853702a1eda896dd1970c097335c233b16b2
Hugo Wojtowicz <92095721+IdiotaInformatyk@users.noreply.github.com>
2021-11-16 23:45:33 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/motto_dnia_aplikacja/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.motto_dnia_aplikacja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
